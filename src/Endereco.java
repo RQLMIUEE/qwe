ea388928e89bc9da6338eee95bf06fe0ede5b677
RQLMIUEE <riquelme.ct.santos@gmail.com>
2025-09-19 10:10:41 -0300

    feito

--- src/Endereco.java
+++ src/Endereco.java
@@ -1,4 +1,43 @@
 
 public class Endereco {
-
+ String ruaAt;
+ String cidadeAt;
+ String estadoAt;
 }
+
+
+public Endereco(String RuaPar, String cidadePar, String estadoPar) {
+	
+	this.ruaAt = ruaPar;
+	this.cidadeAt = cidadePar;
+	this.estadoAt = estadoPar;
+	
+}
+
+public  void setRua() {
+	this.ruaAt = ruaPar;
+}
+
+public String setRua() {
+	return
+}
+
+
+public  void setestado() {
+	this.estadoAt = estadoPar;
+}
+
+public String setestado() {
+	return
+}
+
+public  void setcidade() {
+	this.cidadeAt = cidadePar;
+}
+
+public String setcidade() {
+	return
+}
+
+
+v
